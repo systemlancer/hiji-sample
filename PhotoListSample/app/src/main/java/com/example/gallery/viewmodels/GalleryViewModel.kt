ad50e22eb95dfd3ae567273e58ee3c8a084ddbb6
Yoshifumi <ocarina1025@gmail.com>
2020-08-26 12:32:29 +0900

    init gallery sample

--- PhotoListSample/app/src/main/java/com/example/gallery/viewmodels/GalleryViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.gallery.viewmodels
-
-import androidx.lifecycle.ViewModel
-import com.example.gallery.models.GalleryModel
-
-class GalleryViewModel(private val galleryModel: GalleryModel): ViewModel() {
-}
